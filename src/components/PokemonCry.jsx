376000a2c26d077e0c6da884d6e696b599bb6116
Alberto Romero <albertoromerocabezas@gmail.com>
2025-02-25 01:10:30 +0100

    Add key attribute on PokemonCry audio element

--- src/components/PokemonCry.jsx
+++ src/components/PokemonCry.jsx
@@ -1,6 +1,6 @@
 export const PokemonCry = ({ cry }) => {
     return (
-        <audio controls>
+        <audio key={cry} controls>
             <source src={cry} type="audio/ogg" />
         </audio>
     );
